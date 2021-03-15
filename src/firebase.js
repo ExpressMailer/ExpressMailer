21380d3c4ec31b0583b490ac4754df5fb668aaec
Rugved <rugved.bongale@somaiya.edu>
2021-03-15 17:40:15 +0530

    Renamed and pushed firebase creds

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: process.env.apiKey || "AIzaSyA6rWV50SdKlGnmnBOqfVJ6lu4UeCjtt_U",
+    authDomain: process.env.authDomain || "clone-f427f.firebaseapp.com",
+    projectId: process.env.projectId || "clone-f427f",
+    storageBucket: process.env.storageBucket || "clone-f427f.appspot.com",
+    messagingSenderId: process.env.messagingSenderId || "1086229062538",
+    appId: process.env.appId || "1:1086229062538:web:f3f5437418ee5c1f721e73"
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { db,auth,provider }
