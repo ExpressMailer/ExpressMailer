9c5f5fc161a6e7a6fca296795a3c38815e210875
tushargithub44 <tushar.bapecha@somaiya.edu>
2021-03-19 09:49:35 +0530

    onclick to inbox

--- src/components/SidebarOption/SidebarOption.js
+++ src/components/SidebarOption/SidebarOption.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import styles from './SidebarOption.module.css';
+import { useHistory } from 'react-router-dom';
 
 function SidebarOption({ Icon, title, number, selected}) {
-    return <div className={`${styles.sidebarOption} ${selected && styles.sidebarOption__active}`}>
+    const history = useHistory();
+    return <div className={`${styles.sidebarOption} ${selected && styles.sidebarOption__active}`} onClick={() => history.push('/')}>
         <Icon />
         <h3>{title}</h3>
         <p>{number}</p>   
