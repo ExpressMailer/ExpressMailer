e4de944e7f6e938d600f56aa3bc692834204307a
Rugved <rugved.bongale@somaiya.edu>
2021-04-28 09:05:14 +0530

    Delete working - only sender can delete mails

--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -17,7 +17,7 @@ import { selectOpenMail } from '../../features/mailSlice';
 import { useSelector } from 'react-redux'; 
 import { auth, db } from '../../firebase';
 import ReactHtmlParser from 'react-html-parser';
-import { toggleImportant, toggleSpam } from '../../utilities/utils';
+import { deleteMail, toggleImportant, toggleSpam } from '../../utilities/utils';
 import Loading from '../Loading/Loading';
 import ReactTooltip from 'react-tooltip';
 import { useReactToPrint } from 'react-to-print';
@@ -80,7 +80,18 @@ function Mail() {
                         </p>
                     <ReactTooltip place="bottom"/>
                         <p data-tip="Delete">
-                            <IconButton>
+                            <IconButton onClick={(e) => {
+                                e.stopPropagation()
+                                deleteMail(selectedMail.id).then(result => {
+                                    console.log(result)
+                                    if(result){
+                                        history.push('/')
+                                    }
+                                    else{
+                                        alert('Action Denied')
+                                    }
+                                })
+                            }}>
                                 <DeleteIcon />
                             </IconButton>
                         </p>
